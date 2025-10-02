0b7aedcfe2937d7eaa740fce0d8e99bcf874241c
Carlos Pardo <carlos.pardo.b78@gmail.com>
2025-10-02 13:39:08 -0300

    BASE PROYECTO

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
 from fastapi import FastAPI
+from session import create_database, get_session
+from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan_manager(app: FastAPI):
+    """
+    Función que maneja los eventos de inicio y apagado de la aplicación.
+    """
+    print("Iniciando la aplicación...")
+    create_database()
+    yield
+    print("Cerrando la aplicación...")
 
+app = FastAPI(
+    title="API PUEBLO MADERA",
+    description="API completo de PUEBLO MADERA",
+    version="1.0.0",
+    lifespan=lifespan_manager
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "¡Bienvenido a la API de PUEBLO MADERA!"}
--- session.py
+++ session.py
@@ -1,1 +1,21 @@
 from sqlmodel import create_engine, Session, SQLModel
+
+# Define el nombre del archivo de la base de datos
+sqlite_file_name = "database.db"
+
+# La URL de la base de datos se genera a partir del nombre del archivo
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+# Crea el motor de la base de datos
+# `echo=True` es útil para ver las sentencias SQL que se ejecutan, desactívalo en producción
+engine = create_engine(sqlite_url, echo=True, connect_args={"timeout": 30})
+
+def create_database():
+    """
+    Crea la base de datos y las tablas a partir de los modelos de SQLModel.
+    """
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
